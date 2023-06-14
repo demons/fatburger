e1c907790beab2982280fd044715ecddac61287c
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-14 17:31:15 +0500

    Добавит AddGroupForm

---
+++ app/components/AddGroupForm.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { useGroups } from "../store";
+
+function AddGroupForm() {
+  const [title, setTitle] = useState("");
+  const addGroup = useGroups((state) => state.addGroup);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!title) {
+      return;
+    }
+
+    addGroup(title);
+    setTitle("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button type="submit">Добавить</button>
+    </form>
+  );
+}
+
+export default AddGroupForm;
--- app/components/GroupList.js
+++ app/components/GroupList.js
@@ -1,12 +1,22 @@
 "use client";
 
 import { useGroups } from "../store";
+import AddGroupForm from "./AddGroupForm";
 import Group from "./Group";
 
 function GroupList() {
   const groups = useGroups((state) => state.groups);
 
-  return groups.map((group) => <Group key={group.id} group={group} />);
+  const renderedGroups = groups.map((group) => (
+    <Group key={group.id} group={group} />
+  ));
+
+  return (
+    <div>
+      <div className="groups">{renderedGroups}</div>
+      <AddGroupForm />
+    </div>
+  );
 }
 
 export default GroupList;
