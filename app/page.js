de6eee66552d2f1d14cfd3d97bfc34b95f91f03c
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-15 11:54:17 +0500

    Переименует страницу

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 import GroupList from "./components/GroupList";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function GroupListPage() {
   return (
     <main className={styles.main}>
       <GroupList />
