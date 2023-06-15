6d2e122e78928683416a204d72a37e9170e506ec
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-15 12:59:50 +0500

    Добавит заголовок для GroupItem

--- app/components/GroupItem.js
+++ app/components/GroupItem.js
@@ -1,12 +1,16 @@
 import { useGroups } from "../store";
+import Amount from "./Amount";
 
 function GroupItem({ group }) {
   const removeGroup = useGroups((state) => state.removeGroup);
 
   return (
     <div className="group-item">
-      {group.title}
-      <button onClick={(e) => removeGroup(group.id)}>Удалить</button>
+      <div className="header">
+        <div className="title">{group.title}</div>
+        <Amount />
+        <button onClick={(e) => removeGroup(group.id)}>Удалить</button>
+      </div>
     </div>
   );
 }
