3384cf7d59cb00adca359fae1d048347a2ba8aa4
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-15 23:20:35 +0500

    Рефакторинг подсчета суммы кбжу

--- app/components/Group.js
+++ app/components/Group.js
@@ -7,11 +7,20 @@ function Group({ group, items }) {
 
   const filteredGroupItems = items.filter((item) => item.groupId === group.id);
 
+  let ingredients = [];
+  filteredGroupItems.forEach((item) => {
+    if (item.items) {
+      ingredients = [...ingredients, ...item.items];
+    } else {
+      ingredients = [...ingredients, item];
+    }
+  });
+
   return (
     <div className="group-item">
       <div className="header">
         <div className="title">{group.title}</div>
-        <Amount items={filteredGroupItems} />
+        <Amount items={ingredients} />
         <button onClick={(e) => removeGroup(group.id)}>Удалить</button>
       </div>
       <GroupItemList items={filteredGroupItems} />
