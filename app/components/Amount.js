75b329ebcca96db58c1bd505d2ab878a7c5e730b
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-15 12:42:22 +0500

    Добавит значения Amount

--- app/components/Amount.js
+++ app/components/Amount.js
@@ -1,5 +1,12 @@
 function Amount() {
-  return <div className="amount">Amount</div>;
+  return (
+    <div className="amount">
+      <div className="amount-item">Калории: 50</div>
+      <div className="amount-item">Белки: 40</div>
+      <div className="amount-item">Жиры: 30</div>
+      <div className="amount-item">Углеводы: 20</div>
+    </div>
+  );
 }
 
 export default Amount;
