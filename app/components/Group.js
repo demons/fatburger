26d524a22d7609e35e985ecf01a63159dceffa76
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-15 19:36:05 +0500

    Переименует переменную

--- app/components/Group.js
+++ app/components/Group.js
@@ -5,16 +5,16 @@ import GroupItemList from "./GroupItemList";
 function Group({ group, items }) {
   const { removeGroup } = useGroups();
 
-  const filteredItems = items.filter((item) => item.groupId === group.id);
+  const filteredGroupItems = items.filter((item) => item.groupId === group.id);
 
   return (
     <div className="group-item">
       <div className="header">
         <div className="title">{group.title}</div>
-        <Amount items={filteredItems} />
+        <Amount items={filteredGroupItems} />
         <button onClick={(e) => removeGroup(group.id)}>Удалить</button>
       </div>
-      <GroupItemList items={filteredItems} />
+      <GroupItemList items={filteredGroupItems} />
     </div>
   );
 }
