d3c4191538264d06179e40dd8c2f48692d0bbe23
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-15 12:00:42 +0500

    Обновит страницу

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import GroupList from "./components/GroupList";
 export default function GroupListPage() {
   return (
     <main>
+      <h1>Список групп</h1>
       <GroupList />
     </main>
   );
