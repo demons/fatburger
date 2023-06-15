21dc0138aafdb4045850f82753495546c2371d0b
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-15 11:55:48 +0500

    Удалит стили

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
 import GroupList from "./components/GroupList";
-import styles from "./page.module.css";
 
 export default function GroupListPage() {
   return (
-    <main className={styles.main}>
+    <main>
       <GroupList />
     </main>
   );
