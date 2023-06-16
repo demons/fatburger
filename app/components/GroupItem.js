9d323fc702576b082a734b28869425fec5ef1ef3
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-16 17:22:33 +0500

    Добавит стили для GroupItem

--- app/components/GroupItem.js
+++ app/components/GroupItem.js
@@ -4,8 +4,10 @@ function GroupItem({ groupItem, index }) {
   const ingredients = groupItem.items ? groupItem.items : [groupItem];
 
   return (
-    <div>
-      {index}. {groupItem.title} <AmountItem items={ingredients} />
+    <div className="group-item">
+      <div className="header">
+        {index}. {groupItem.title} <AmountItem items={ingredients} />
+      </div>
     </div>
   );
 }
