0c3131a164a2a76b20d5035b1b5a6a81c42f307d
Shishkin Aleksandr <shishkin.aleksandr88@gmail.com>
2023-06-16 23:28:02 +0500

    Добавит старницу редактирования Group

--- app/components/Group.js
+++ app/components/Group.js
@@ -1,8 +1,10 @@
+import { useRouter } from "next/navigation";
 import { useGroups } from "../store";
 import AmountItem from "./AmountItem";
 import GroupItemList from "./GroupItemList";
 
 function Group({ group }) {
+  const router = useRouter();
   const { getGroupItemsByGroupId, getIngredientsByGroupId } = useGroups();
 
   const groupItems = getGroupItemsByGroupId(group.id);
@@ -10,7 +12,7 @@ function Group({ group }) {
   let ingredients = getIngredientsByGroupId(group.id);
 
   return (
-    <div className="group">
+    <div className="group" onClick={() => router.push(`/group/${group.id}`)}>
       <div className="header">
         <div className="title">{group.title}</div>
         <AmountItem items={ingredients} />
---
+++ app/group/[id]/page.js
@@ -0,0 +1,3 @@
+export default function Page({ params }) {
+  return <div>Group {params.id}</div>;
+}
