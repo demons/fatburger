cd3964fe9e69cd6b1f42030472274ea5c6821c43
Shishkin Aleksandr <shishkin.aleksandr88@gmail.com>
2023-06-17 12:58:17 +0500

    Добавит заголовок для EditGroup

--- app/components/EditGroup.js
+++ app/components/EditGroup.js
@@ -1,8 +1,20 @@
+"use client";
+
+import { useGroups } from "../store";
+import AmountItem from "./AmountItem";
 import GroupItemList from "./GroupItemList";
 
 export default function EditGroup({ groupId }) {
+  const { getGroupById, getIngredientsByGroupId } = useGroups();
+
+  const group = getGroupById(groupId);
+
   return (
-    <div>
+    <div className="edit-group">
+      <div className="header">
+        <div className="title">{group.title}</div>
+        <AmountItem items={getIngredientsByGroupId(groupId)} />
+      </div>
       <GroupItemList groupId={groupId} />
     </div>
   );
