15e8e70cd501e1a356635160966393032f619a83
Shishkin Aleksandr <shishkin.aleksandr88@gmail.com>
2023-06-17 13:04:34 +0500

    Добавит кнопку возврата на главную страницу

--- app/components/EditGroup.js
+++ app/components/EditGroup.js
@@ -1,16 +1,19 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useGroups } from "../store";
 import AmountItem from "./AmountItem";
 import GroupItemList from "./GroupItemList";
 
 export default function EditGroup({ groupId }) {
+  const router = useRouter();
   const { getGroupById, getIngredientsByGroupId, getIngredients } = useGroups();
 
   const group = getGroupById(groupId);
 
   return (
     <div className="edit-group">
+      <button onClick={() => router.push(`/`)}>Готово</button>
       <AmountItem items={getIngredients()} />
       <div className="header">
         <div className="title">{group.title}</div>
