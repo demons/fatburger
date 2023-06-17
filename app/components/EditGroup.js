56cc2ecc773f546906bbd404fe19e93232bec046
Shishkin Aleksandr <shishkin.aleksandr88@gmail.com>
2023-06-17 13:01:09 +0500

    Добавит вывод общей суммы кбжу

--- app/components/EditGroup.js
+++ app/components/EditGroup.js
@@ -5,12 +5,13 @@ import AmountItem from "./AmountItem";
 import GroupItemList from "./GroupItemList";
 
 export default function EditGroup({ groupId }) {
-  const { getGroupById, getIngredientsByGroupId } = useGroups();
+  const { getGroupById, getIngredientsByGroupId, getIngredients } = useGroups();
 
   const group = getGroupById(groupId);
 
   return (
     <div className="edit-group">
+      <AmountItem items={getIngredients()} />
       <div className="header">
         <div className="title">{group.title}</div>
         <AmountItem items={getIngredientsByGroupId(groupId)} />
