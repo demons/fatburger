73842e32fc8a412de20d80334545b0642d49f253
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-18 00:31:30 +0500

    Создаст страницу добавления ингредиента в группу

--- app/groups/[id]/addIngredient/page.js
+++ app/groups/[id]/addIngredient/page.js
@@ -1,3 +1,37 @@
+"use client";
+
+import { useGroups } from "@/store";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 export default function Page({ params }) {
-  return <div>Add Ingredient: {params.id}</div>;
+  const { products, addGroupItem } = useGroups();
+  const [count, setCount] = useState(0);
+  const router = useRouter();
+
+  const groupId = params.id;
+
+  const renderedProducts = products.map((product) => {
+    return <div key={product.id}>{product.title}</div>;
+  });
+
+  return (
+    <div>
+      {renderedProducts}
+      <input
+        type="text"
+        value={count}
+        onChange={(e) => setCount(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          addGroupItem(groupId, "1", count);
+
+          router.push(`/groups/${groupId}`);
+        }}
+      >
+        Добавить
+      </button>
+    </div>
+  );
 }
