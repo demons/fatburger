5cc579503f0b16221d2f53510791037bce81c8aa
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-18 16:09:15 +0500

    Обноавит страницу редактирования блюда

--- app/groups/[groupId]/dishTemplates/[dishTemplateId]/page.js
+++ app/groups/[groupId]/dishTemplates/[dishTemplateId]/page.js
@@ -1,9 +1,40 @@
+"use client";
+
+import { useGroups } from "@/store";
+import { useEffect, useState } from "react";
+
 export default function Page({ params }) {
+  const [dish, setDish] = useState({ ingredients: [] });
+
   const { groupId, dishTemplateId } = params;
+  const { createDishFromDishTemplate, getProductById } = useGroups();
+
+  useEffect(() => {
+    setDish(createDishFromDishTemplate(dishTemplateId));
+  }, []);
+
+  const handleRemove = (ingredientId) => {
+    dish.ingredients = dish.ingredients.filter(
+      (ingredient) => ingredient.id !== ingredientId
+    );
+    setDish({ ...dish });
+  };
+
+  const renderedIngredients = dish.ingredients.map((ingredient) => {
+    const product = getProductById(ingredient.productId);
+
+    return (
+      <div key={ingredient.id}>
+        {product.title}
+        <button onClick={() => handleRemove(ingredient.id)}>Удалить</button>
+      </div>
+    );
+  });
 
   return (
     <div>
-      GroupId: {groupId}, dishTemplateId: {dishTemplateId}
+      {dish.title}
+      <div>{renderedIngredients}</div>
     </div>
   );
 }
