234c39873840a9a1c0749aacf978f3133946c27a
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-18 11:02:27 +0500

    Изменит структуру страниц

--- app/groups/[id]/page.js
+++ app/groups/[id]/page.js
@@ -28,9 +28,7 @@ export default function Page({ params }) {
       </div>
       <GroupItemList groupId={groupId} isEditable={true} />
       <div className="buttons">
-        <Link href={`/groups/${groupId}/addIngredient`}>
-          Добавить ингредиент
-        </Link>
+        <Link href={`/groups/${groupId}/products`}>Добавить ингредиент</Link>
         <button>Добавить блюдо</button>
       </div>
     </div>
