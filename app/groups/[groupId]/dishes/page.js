7f4bdbcd8456ed69c64a34ee60de649af81ff161
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-18 12:47:00 +0500

    Добавит список шаблонов блюд

--- app/groups/[groupId]/dishes/page.js
+++ app/groups/[groupId]/dishes/page.js
@@ -1,3 +1,16 @@
+"use client";
+
+import { useGroups } from "@/store";
+
 export default function Page({ params }) {
-  return "Dishes";
+  const { dishTemplates } = useGroups();
+
+  const renderedDishTemplates = dishTemplates.map((dishTemplate) => (
+    <div key={dishTemplate.id} className="dish-template">
+      {dishTemplate.title}
+      <button>Добавить</button>
+    </div>
+  ));
+
+  return <div className="dish-templates">{renderedDishTemplates}</div>;
 }
