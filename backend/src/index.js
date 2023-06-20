f2e7d9a8f0615c726f0dcb4100456c39d8a43238
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-20 11:59:23 +0300

    Добавит новые api

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,31 @@ const express = require("express");
 const app = express();
 const port = 8000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+let groups = [
+  { id: "1", title: "Завтрак", groupItems: [] },
+  { id: "2", title: "Обед", groupItems: [] },
+];
+
+app.get("/groups", (req, res) => {
+  return res.send(groups);
+});
+
+app.post("/groups", (req, res) => {
+  const { title } = req.body;
+
+  const newGroup = {
+    id: groups.length + 1,
+    title,
+    groupItems: [],
+  };
+  groups.push(newGroup);
+  return res.send(newGroup);
 });
 
 app.listen(port, () => {
