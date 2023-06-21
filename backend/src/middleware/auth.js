1229b1584c11c80b42d25d4f01449aedd1f6f161
demons <shishkin.aleksandr88@gmail.com>
2023-06-21 10:15:20 +0000

    Добавит проверку accessToken в auth middleware

--- backend/src/middleware/auth.js
+++ backend/src/middleware/auth.js
@@ -3,12 +3,8 @@ const tokenService = require("../services/tokenService");
 
 module.exports = function (req, res, next) {
   try {
-    const authorizationHeader = req.headers.authorization;
-    if (!authorizationHeader) {
-      return next(ApiError.unauthorizedError());
-    }
+    const accessToken = req.cookies.accessToken;
 
-    const accessToken = authorizationHeader.split(" ")[1];
     if (!accessToken) {
       return next(ApiError.unauthorizedError());
     }
