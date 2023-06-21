60a1d273d38cbbbb0a896c308a4999798a87d4d6
demons <shishkin.aleksandr88@gmail.com>
2023-06-21 10:37:24 +0000

    Удалит поле

--- backend/src/dtos/userDto.js
+++ backend/src/dtos/userDto.js
@@ -2,12 +2,10 @@ module.exports = class UserDto {
   email;
   id;
   isActivated;
-  name;
 
   constructor(model) {
     this.email = model.email;
     this.id = model.id;
     this.isActivated = model.isActivated;
-    this.name = model.name;
   }
 };
