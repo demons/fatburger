7cea6b0c6249745c2316a39a3a74fc1ceecce7cb
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-21 17:01:32 +0500

    Добавит обертку fetch

--- frontend/services/index.js
+++ frontend/services/index.js
@@ -1,19 +1,25 @@
-export async function getGroups() {
-  const res = await fetch(`/api/groups`);
+export async function fetchBase(url, options = {}) {
+  const res = await fetch(url, options);
+
+  if (res.status === 401) {
+    location.href = "/login";
+  }
 
   if (!res.ok) throw new Error("Произошла ошибка при получении...");
 
   return res.json();
 }
 
+export async function getGroups() {
+  return fetchBase(`/api/groups`);
+}
+
 export async function createGroup(title) {
-  const res = await fetch(`/api/groups`, {
+  return fetch(`/api/groups`, {
     method: "POST",
     body: JSON.stringify({ title }),
     headers: {
       "Content-Type": "application/json",
     },
   });
-
-  return res.json();
 }
