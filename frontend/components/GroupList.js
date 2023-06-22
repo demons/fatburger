fac0a6c11ae66fd6c5debb51bd42553198b94176
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-22 10:54:41 +0500

    Обновит компонент GroupList

--- frontend/components/GroupList.js
+++ frontend/components/GroupList.js
@@ -1,26 +1,19 @@
 "use client";
 
-import { useGroups } from "@/store";
 import Group from "./Group";
 import { useGroupsQuery } from "@/hooks";
 
 function GroupList() {
-  const { data, isLoading, isSuccess } = useGroupsQuery();
+  const { data: groups, status } = useGroupsQuery();
 
-  if (isLoading) {
-    return "Loading...";
+  switch (status) {
+    case "loading": {
+      return "Loading...";
+    }
+    case "success": {
+      return groups.map((group) => <Group key={group.id} group={group} />);
+    }
   }
-
-  return (
-    isSuccess && data.map((group) => <div key={group.id}>{group.title}</div>)
-  );
-  const groups = useGroups((state) => state.groups);
-
-  const renderedGroups = groups.map((group) => (
-    <Group key={group.id} group={group} />
-  ));
-
-  return <div className="group-list">{renderedGroups}</div>;
 }
 
 export default GroupList;
