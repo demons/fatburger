4f40abeb57aae11a89ce2f593f5df44913223091
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-23 19:30:30 +0500

    Добавит toBoolean()

--- backend/src/utils.js
+++ backend/src/utils.js
@@ -8,6 +8,26 @@ const asyncWrapper = (cb) => {
   };
 };
 
+const toBoolean = (value, defaultValue = false) => {
+  if (!value) {
+    return defaultValue;
+  }
+
+  switch (value.toLowerCase()) {
+    case "0":
+      return false;
+    case "false":
+      return false;
+    case "true":
+      return true;
+    case "1":
+      return true;
+    default:
+      return defaultValue;
+  }
+};
+
 module.exports = {
   asyncWrapper,
+  toBoolean,
 };
