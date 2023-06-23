3b7895b2c019a86338d73820c7b5c48f1f960dfe
demons <shishkin.aleksandr88@gmail.com>
2023-06-23 04:54:40 +0000

    Обновит модель Group

--- backend/src/db/models/group.js
+++ backend/src/db/models/group.js
@@ -1,11 +1,15 @@
 "use strict";
+
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Group extends Model {
     static associate(models) {
-      // define association here
+      const { Group, Ingredient } = models;
+      Group.hasMany(Ingredient, { foreignKey: { allowNull: false } });
     }
   }
+
   Group.init(
     {
       title: {
@@ -18,5 +22,6 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "group",
     }
   );
+
   return Group;
 };
