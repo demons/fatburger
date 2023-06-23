9b57a8de59797c5c17ad256fa04d77ee8f5f8db5
demons <shishkin.aleksandr88@gmail.com>
2023-06-23 04:54:11 +0000

    Обновит модель Product

--- backend/src/db/models/product.js
+++ backend/src/db/models/product.js
@@ -1,23 +1,26 @@
 "use strict";
+
 const { Model } = require("sequelize");
+
 module.exports = (sequelize, DataTypes) => {
   class Product extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      const { Ingredient, Product } = models;
+      Product.hasOne(Ingredient);
     }
   }
+
   Product.init(
     {
       title: {
         type: DataTypes.STRING(100),
         allowNull: false,
       },
-      maker: DataTypes.STRING(100),
+      maker: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+        defaultValue: "",
+      },
       energy: {
         type: DataTypes.INTEGER,
         allowNull: false,
@@ -40,5 +43,6 @@ module.exports = (sequelize, DataTypes) => {
       modelName: "product",
     }
   );
+
   return Product;
 };
