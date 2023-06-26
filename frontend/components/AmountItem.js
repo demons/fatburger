acb5cd8b92b8e52e30492f604bd805297d84c53b
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-26 18:57:34 +0500

    Обновит AmountItem

--- frontend/components/AmountItem.js
+++ frontend/components/AmountItem.js
@@ -1,19 +1,5 @@
-function AmountItem({ items = [] }) {
-  const initAmount = {
-    energy: 0,
-    protein: 0,
-    fat: 0,
-    carb: 0,
-  };
-
-  const { energy, protein, fat, carb } = items.reduce((prev, curr) => {
-    return {
-      energy: prev.energy + curr.energy,
-      protein: prev.protein + curr.protein,
-      fat: prev.fat + curr.fat,
-      carb: prev.carb + curr.carb,
-    };
-  }, initAmount);
+function AmountItem({ amount }) {
+  const { energy, protein, fat, carb } = amount;
 
   return (
     <div className="amount-item">
