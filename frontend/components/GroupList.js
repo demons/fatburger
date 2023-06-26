592bb2179939f5fba88fe04d503ddcc23958c51e
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-26 18:58:12 +0500

    Обновит GroupList

--- frontend/components/GroupList.js
+++ frontend/components/GroupList.js
@@ -1,19 +1,11 @@
 "use client";
 
 import Group from "./Group";
-import { useGroupsQuery } from "@/hooks";
 
-function GroupList() {
-  const { data: groups, status } = useGroupsQuery();
-
-  switch (status) {
-    case "loading": {
-      return "Loading...";
-    }
-    case "success": {
-      return groups.map((group) => <Group key={group.id} group={group} />);
-    }
-  }
+function GroupList({ groups }) {
+  return groups.map((group) => {
+    return <Group key={group.id} group={group} />;
+  });
 }
 
 export default GroupList;
