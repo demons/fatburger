1d837f23c9b97fa92ad79216ba2362a58e77a5c7
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-27 14:55:03 +0500

    Рефакторинг функционала добавления ингредиента

--- frontend/app/groups/[groupId]/ingredients/page.js
+++ frontend/app/groups/[groupId]/ingredients/page.js
@@ -1,35 +1,7 @@
-"use client";
-
-import { useAddIngredient, useProductsQuery } from "@/hooks";
-import { useRouter } from "next/navigation";
+import SelectProduct from "@/components/SelectProduct";
 
 export default function Page({ params }) {
   const { groupId } = params;
-  const { data, isLoading, isError } = useProductsQuery();
-  const { mutate: addIngredient } = useAddIngredient();
-  const router = useRouter();
 
-  if (isLoading) {
-    return "Loading...";
-  }
-
-  if (isError) {
-    return "Произошла ошибка";
-  }
-
-  const handleAdd = (productId) => {
-    addIngredient({ groupId, productId, count: 0 });
-    router.push(`/groups/${groupId}`);
-  };
-
-  const renderedProducts = data.map((product) => {
-    return (
-      <div key={product.id}>
-        {product.title}
-        <button onClick={() => handleAdd(product.id)}>Добавить</button>
-      </div>
-    );
-  });
-
-  return <div>{renderedProducts}</div>;
+  return <SelectProduct groupId={groupId} />;
 }
