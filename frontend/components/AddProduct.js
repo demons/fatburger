785027591752d97d3b8376fa719f9b45c1d63fd7
demons <shishkin.aleksandr88@gmail.com>
2023-06-27 14:05:15 +0000

    Перенесет функционал в компонент AddProduct

---
+++ frontend/components/AddProduct.js
@@ -0,0 +1,146 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useAddProduct } from "@/hooks";
+
+export default function AddProduct({ product }) {
+  const [title, setTitle] = useState("");
+  const [maker, setMaker] = useState("");
+  const [energy, setEnergy] = useState("");
+  const [protein, setProtein] = useState("");
+  const [fat, setFat] = useState("");
+  const [carb, setCarb] = useState("");
+  const router = useRouter();
+  const { mutate: addProduct } = useAddProduct();
+
+  const handleChange = (e) => {
+    let { value, type } = e.target;
+
+    if (type.toLowerCase() === "number") {
+      value = value.replace(",", ".");
+    }
+
+    switch (e.target.name) {
+      case "title":
+        {
+          setTitle(value);
+        }
+        break;
+      case "maker":
+        {
+          setMaker(value);
+        }
+        break;
+      case "energy":
+        {
+          setEnergy(value);
+        }
+        break;
+      case "protein":
+        {
+          setProtein(value);
+        }
+        break;
+      case "fat":
+        {
+          setFat(value);
+        }
+        break;
+      case "carb":
+        {
+          setCarb(value);
+        }
+        break;
+    }
+  };
+
+  const handleCancel = () => {
+    router.push(`/products`);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const product = {
+      title,
+      maker: maker || "",
+      energy: energy || 0,
+      protein: protein || 0,
+      fat: fat || 0,
+      carb: carb || 0,
+    };
+    addProduct(product);
+    router.push(`/products`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="title">Название</label>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="maker">Производитель</label>
+        <input
+          type="text"
+          name="maker"
+          id="maker"
+          value={maker}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="energy">Калории</label>
+        <input
+          type="number"
+          name="energy"
+          id="energy"
+          value={energy}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="protein">Белки</label>
+        <input
+          type="number"
+          name="protein"
+          id="protein"
+          value={protein}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="fat">Жиры</label>
+        <input
+          type="number"
+          name="fat"
+          id="fat"
+          value={fat}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+        <label htmlFor="carb">Углеводы</label>
+        <input
+          type="number"
+          name="carb"
+          id="carb"
+          value={carb}
+          onChange={handleChange}
+        />
+      </div>
+
+      <button type="submit">Добавить</button>
+      <button type="button" onClick={handleCancel}>
+        Отмена
+      </button>
+    </form>
+  );
+}
