66592a054838eacbcee9de858034c56053d4c80d
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-27 12:35:47 +0500

    Добавит кнопки редактирования ингредиента

--- frontend/components/GroupItem.js
+++ frontend/components/GroupItem.js
@@ -1,14 +1,29 @@
 import { useDeleteDish, useDeleteIngredientFromGroup } from "@/hooks";
 import AmountItem from "./AmountItem";
+import { useState } from "react";
 
 function GroupItem({ groupItem, index }) {
+  const [state, setState] = useState("");
   const { mutate: deleteDish } = useDeleteDish();
   const { mutate: deleteIngredient } = useDeleteIngredientFromGroup();
   const { energy, protein, fat, carb } = groupItem;
   const amount = { energy, protein, fat, carb };
+  const { groupId, dishId, ingredientId } = groupItem;
+
+  const handleEditApply = () => {
+    console.log("apply");
+    setState("");
+  };
+
+  const handleEditCancel = () => {
+    setState("");
+  };
+
+  const handleEditClick = () => {
+    setState("editIngredient");
+  };
 
   const deleteGroupItem = () => {
-    const { groupId, dishId, ingredientId } = groupItem;
     if (dishId) {
       deleteDish({ groupId, dishId });
     } else if (ingredientId) {
@@ -16,14 +31,35 @@ function GroupItem({ groupItem, index }) {
     }
   };
 
-  return (
-    <div className="group-item">
-      <div className="header">
-        {index}. {groupItem.title} <AmountItem amount={amount} />
-        <button onClick={deleteGroupItem}>Удалить</button>
-      </div>
-    </div>
-  );
+  let content;
+
+  switch (state) {
+    case "editIngredient":
+      {
+        content = (
+          <div>
+            {index}. {groupItem.title}
+            <input type="text" />
+            <button onClick={handleEditApply}>Изменить</button>
+            <button onClick={handleEditCancel}>Отмена</button>
+          </div>
+        );
+      }
+      break;
+    default: {
+      content = (
+        <>
+          <div className="header">
+            {index}. {groupItem.title} <AmountItem amount={amount} />
+            <button onClick={handleEditClick}>Редактировать</button>
+            <button onClick={deleteGroupItem}>Удалить</button>
+          </div>
+        </>
+      );
+    }
+  }
+
+  return <div className="group-item">{content}</div>;
 }
 
 export default GroupItem;
