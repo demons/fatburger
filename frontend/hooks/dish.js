632f159cfacf6d446a44fd5616a27c16c30cb458
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-30 02:38:35 +0500

    Обновит хуки
    Добавит функционал работы с ингредиентами блюда

--- frontend/hooks/dish.js
+++ frontend/hooks/dish.js
@@ -1,4 +1,10 @@
-import { addDish, fetchDish } from "@/services/dish";
+import {
+  addDish,
+  addIngredient,
+  deleteIngredient,
+  editIngredient,
+  fetchDish,
+} from "@/services/dish";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useAddDish() {
@@ -15,6 +21,45 @@ export function useDishQuery(groupId, dishId) {
     queryFn: () => fetchDish(groupId, dishId),
     onError: (e) => {
       alert(e.message);
+    },
+  });
+}
+
+export function useAddIngredient() {
+  const client = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ groupId, dishId, productId, count }) => {
+      return addIngredient(groupId, dishId, productId, count);
+    },
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ["dish"] });
+    },
+  });
+}
+
+export function useEditIngredient() {
+  const client = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ groupId, dishId, ingredientId, productId, count }) => {
+      return editIngredient(groupId, dishId, ingredientId, productId, count);
+    },
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ["dish"] });
+    },
+  });
+}
+
+export function useDeleteIngredient() {
+  const client = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({ groupId, dishId, ingredientId }) => {
+      return deleteIngredient(groupId, dishId, ingredientId);
+    },
+    onSuccess: () => {
+      client.invalidateQueries({ queryKey: ["dish"] });
     },
   });
 }
