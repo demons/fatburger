34773497951f4ebc7b49e0f0217060430e1a5baf
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-06-30 02:37:58 +0500

    Обновит сервисы
    Добавит функционал работы с ингредиентами блюда

--- frontend/services/dish.js
+++ frontend/services/dish.js
@@ -10,3 +10,35 @@ export async function addDish(groupId, dishTemplateId) {
 export async function fetchDish(groupId, dishId) {
   return fetchBase(`/groups/${groupId}/dishes/${dishId}`);
 }
+
+export async function addIngredient(groupId, dishId, productId, count = 0) {
+  return fetchBase(`/groups/${groupId}/dishes/${dishId}/ingredients`, {
+    method: "POST",
+    body: JSON.stringify({ productId, count }),
+  });
+}
+
+export async function editIngredient(
+  groupId,
+  dishId,
+  ingredientId,
+  productId,
+  count
+) {
+  return fetchBase(
+    `/groups/${groupId}/dishes/${dishId}/ingredients/${ingredientId}`,
+    {
+      method: "PUT",
+      body: JSON.stringify({ productId, count }),
+    }
+  );
+}
+
+export async function deleteIngredient(groupId, dishId, ingredientId) {
+  return fetchBase(
+    `/groups/${groupId}/dishes/${dishId}/ingredients/${ingredientId}`,
+    {
+      method: "DELETE",
+    }
+  );
+}
