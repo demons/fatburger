d91bfa050ca167247063bd6b99afe9dc93e126c1
demons <shishkin.aleksandr88@gmail.com>
2023-07-01 11:28:32 +0000

    Добавит Spinner на главную страницу

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -5,12 +5,13 @@ import AddGroupForm from "@/components/AddGroupForm";
 import AmountItem from "@/components/AmountItem";
 import GroupList from "@/components/GroupList";
 import { useGroupsQuery } from "@/hooks";
+import Spinner from "@/components/Spinner";
 
 export default function GroupListPage() {
   const { data, isLoading, isError } = useGroupsQuery();
 
   if (isLoading) {
-    return "Loading...";
+    return <Spinner />;
   }
 
   if (isError) {
