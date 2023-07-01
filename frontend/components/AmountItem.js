bf83e8232111784344701c0a0be73efda1122453
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-01 11:54:36 +0500

    Обновит AmountItem

--- frontend/components/AmountItem.js
+++ frontend/components/AmountItem.js
@@ -1,13 +1,17 @@
+import { Flex, Box, Container } from "@chakra-ui/react";
+
 function AmountItem({ amount }) {
   const { energy, protein, fat, carb } = amount;
 
   return (
-    <div className="amount-item">
-      <div className="field">Калории: {energy}</div>
-      <div className="field">Белки: {protein}</div>
-      <div className="field">Жиры: {fat}</div>
-      <div className="field">Углеводы: {carb}</div>
-    </div>
+    <Container>
+      <Flex justifyContent="space-between" p="1">
+        <Box>Калории: {energy}</Box>
+        <Box>Белки: {protein}</Box>
+        <Box>Жиры: {fat}</Box>
+        <Box>Углеводы: {carb}</Box>
+      </Flex>
+    </Container>
   );
 }
 
