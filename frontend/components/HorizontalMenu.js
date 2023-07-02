2a7ae4562c2257e751c5fa750c17d77427435def
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-02 22:21:12 +0500

    Добавит HorizontalMenu

---
+++ frontend/components/HorizontalMenu.js
@@ -0,0 +1,18 @@
+import NextLink from "next/link";
+import { HStack, Link } from "@chakra-ui/react";
+
+export default function HorizontalMenu() {
+  return (
+    <HStack>
+      <Link href="/" as={NextLink}>
+        Главная
+      </Link>
+      <Link href="/products" as={NextLink}>
+        Продукты
+      </Link>
+      <Link href="/dishTemplates" as={NextLink}>
+        Блюда
+      </Link>
+    </HStack>
+  );
+}
