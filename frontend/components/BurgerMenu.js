e316d6275db9ec60ced9155e56c39066544e81de
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-02 22:17:00 +0500

    Добавит BurgerMenu

---
+++ frontend/components/BurgerMenu.js
@@ -0,0 +1,36 @@
+import NextLink from "next/link";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  IconButton,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+export default function BurgerMenu() {
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label="Options"
+        icon={<HamburgerIcon />}
+        variant="outline"
+      />
+      <MenuList>
+        <MenuItem as={NextLink} href={`/`}>
+          Главная
+        </MenuItem>
+        <MenuItem as={NextLink} href={`/products`}>
+          Продукты
+        </MenuItem>
+        <MenuItem as={NextLink} href={`/dishTemplates`}>
+          Блюда
+        </MenuItem>
+        <MenuItem as={NextLink} href={`/logout`}>
+          Выйти
+        </MenuItem>
+      </MenuList>
+    </Menu>
+  );
+}
