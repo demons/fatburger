b7db32fc49f92e2d7cfc5958cbcea528115b0bf4
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-03 00:52:50 +0500

    Добавит сервисы для работы с историей

---
+++ frontend/services/story.js
@@ -0,0 +1,33 @@
+import { fetchBase } from ".";
+
+export async function fetchStories() {
+  return fetchBase(`/stories`);
+}
+
+export async function createStory(date, energy, protein, fat, carb, comment) {
+  return fetchBase(`/stories`, {
+    method: "POST",
+    body: JSON.stringify({ date, energy, protein, fat, carb, comment }),
+  });
+}
+
+export async function updateStory(
+  storyId,
+  date,
+  energy,
+  protein,
+  fat,
+  carb,
+  comment
+) {
+  return fetchBase(`/stories/${storyId}`, {
+    method: "PUT",
+    body: JSON.stringify({ date, energy, protein, fat, carb, comment }),
+  });
+}
+
+export async function deleteStory(storyId) {
+  return fetchBase(`/stories/${storyId}`, {
+    method: "DELETE",
+  });
+}
