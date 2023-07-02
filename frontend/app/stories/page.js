7ad0d68db9e80f866901530690ccfe8dfba3bc91
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-03 00:53:20 +0500

    Обновит страницу дневника

--- frontend/app/stories/page.js
+++ frontend/app/stories/page.js
@@ -1,3 +1,61 @@
-export default function Page({ Params }) {
-  return "Дневник";
+"use client";
+
+import { Box, Flex, Stack, Text, HStack, IconButton } from "@chakra-ui/react";
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import ErrorAlert from "@/components/ErrorAlert";
+import Spinner from "@/components/Spinner";
+import { useGroupsQuery } from "@/hooks";
+import { useDeleteStory, useStoriesQuery } from "@/hooks/story";
+import AmountItem from "@/components/AmountItem";
+
+export default function Page({ params }) {
+  const { data: stories, status, error } = useStoriesQuery();
+  const { data: groups } = useGroupsQuery();
+  const { mutate: deleteStory } = useDeleteStory();
+
+  if (status === "loading") {
+    return <Spinner />;
+  }
+
+  if (status === "error") {
+    return <ErrorAlert />;
+  }
+
+  const handleEdit = () => {};
+
+  const handleDelete = (storyId) => {
+    deleteStory({ storyId });
+  };
+
+  const renderedStories = stories.map((story) => {
+    const { id, date, comment, ...amount } = story;
+    return (
+      <Flex
+        key={id}
+        justifyContent="space-between"
+        alignItems="center"
+        border="1px"
+        borderColor="gray.200"
+        my="2"
+        p="2"
+      >
+        <Stack>
+          <Text>{date}</Text>
+          <AmountItem amount={amount} />
+        </Stack>
+        <Text>{comment}</Text>
+        <HStack>
+          <IconButton onClick={handleEdit} size="sm" icon={<EditIcon />} />
+          <IconButton
+            onClick={() => handleDelete(id)}
+            size="sm"
+            colorScheme="red"
+            icon={<DeleteIcon />}
+          />
+        </HStack>
+      </Flex>
+    );
+  });
+
+  return <Box>{renderedStories}</Box>;
 }
