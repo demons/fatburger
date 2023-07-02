0507fe74c0fc3194e2eea087b9ff929bda00c03a
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-02 14:37:46 +0500

    Добавит Amount

---
+++ frontend/components/Amount.js
@@ -0,0 +1,28 @@
+import { Flex, Box, Skeleton, Text } from "@chakra-ui/react";
+import { useStore } from "@/store";
+
+export default function Amount() {
+  const amount = useStore((state) => state.amount);
+
+  return (
+    <Flex
+      justifyContent="space-between"
+      border="1px"
+      borderColor="gray.200"
+      p="2"
+    >
+      <Skeleton isLoaded={amount}>
+        <Text fontSize="sm">Калории: {amount && amount.energy}</Text>
+      </Skeleton>
+      <Skeleton isLoaded={amount}>
+        <Text fontSize="sm">Белки: {amount && amount.protein}</Text>
+      </Skeleton>
+      <Skeleton isLoaded={amount}>
+        <Text fontSize="sm">Жиры: {amount && amount.fat}</Text>
+      </Skeleton>
+      <Skeleton isLoaded={amount}>
+        <Text fontSize="sm">Углеводы: {amount && amount.carb}</Text>
+      </Skeleton>
+    </Flex>
+  );
+}
