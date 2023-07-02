ad24a6bb2e182a134260ae065fbd1a3c4b8a3a71
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-02 19:36:10 +0500

    Обновит SelectProduct

--- frontend/components/SelectProduct.js
+++ frontend/components/SelectProduct.js
@@ -1,8 +1,10 @@
 "use client";
 
+import { Flex, Text, HStack } from "@chakra-ui/react";
 import { useProductsQuery } from "@/hooks";
 import Spinner from "./Spinner";
 import ErrorAlert from "./ErrorAlert";
+import Button from "./Button";
 
 export default function SelectProduct({ onApply }) {
   const { data, status, error } = useProductsQuery();
@@ -16,11 +18,25 @@ export default function SelectProduct({ onApply }) {
   }
 
   const renderedProducts = data.map((product) => {
+    const { id, title, maker } = product;
     return (
-      <div key={product.id}>
-        {product.title}
-        <button onClick={() => onApply(product.id)}>Выбрать</button>
-      </div>
+      <Flex
+        key={id}
+        justifyContent="space-between"
+        alignItems="center"
+        my="2"
+        border="1px"
+        borderColor="gray.200"
+        p="2"
+      >
+        <HStack>
+          <Text as="b">{title}</Text>
+          <Text as="span" fontSize="xs">
+            {maker}
+          </Text>
+        </HStack>
+        <Button onClick={() => onApply(id)}>Выбрать</Button>
+      </Flex>
     );
   });
 
