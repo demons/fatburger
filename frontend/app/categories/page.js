4f2b20dcd542bf229fdfd89b8db2596673029674
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-15 23:08:18 +0500

    Доработает списко категорий

--- frontend/app/categories/page.js
+++ frontend/app/categories/page.js
@@ -1,11 +1,20 @@
 "use client";
 
+import EditTitleForm from "@/components/EditTitleForm";
 import Spinner from "@/components/Spinner";
 import Item from "@/components/common/Item";
-import { useCategoriesQuery } from "@/hooks/category";
+import {
+  useCategoriesQuery,
+  useDeleteCategory,
+  useUpdateCategory,
+} from "@/hooks/category";
+import { useState } from "react";
 
 export default function Page() {
+  const [editId, setEditId] = useState(null);
   const { data, status, error } = useCategoriesQuery();
+  const { mutate: updateCategory } = useUpdateCategory();
+  const { mutate: deleteCategory } = useDeleteCategory();
 
   if (status === "loading") {
     return <Spinner />;
@@ -15,8 +24,34 @@ export default function Page() {
     return <ErrorAlert message={error.message} />;
   }
 
-  const renderedCategories = data.map((category) => {
-    return <Item key={category.id} />;
+  const handleTitleClick = (categoryId) => {
+    setEditId(categoryId);
+  };
+
+  const handleEditApply = (categoryId, title) => {
+    updateCategory({ categoryId, title });
+    setEditId(null);
+  };
+
+  const handleDelete = (categoryId) => {
+    deleteCategory({ categoryId });
+  };
+
+  const renderedCategories = data.map(({ id, title }) => {
+    const actions = {
+      handleTitleClick: () => handleTitleClick(id),
+      handleDelete: () => handleDelete(id),
+    };
+    const titleContent =
+      editId === id ? (
+        <EditTitleForm
+          title={title}
+          onApply={(title) => handleEditApply(id, title)}
+        />
+      ) : (
+        title
+      );
+    return <Item key={id} title={titleContent} actions={actions} />;
   });
 
   return (
