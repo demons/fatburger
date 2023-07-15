2cf3cc9f7a7e06802752c54288973cff0ab922ff
demons <shishkin.aleksandr88@gmail.com>
2023-07-15 17:21:40 +0000

    Добавит вывод списка категорий

--- frontend/app/categories/page.js
+++ frontend/app/categories/page.js
@@ -1,7 +1,28 @@
+"use client";
+
+import Spinner from "@/components/Spinner";
+import Item from "@/components/common/Item";
+import { useCategoriesQuery } from "@/hooks/category";
+
 export default function Page() {
+  const { data, status, error } = useCategoriesQuery();
+
+  if (status === "loading") {
+    return <Spinner />;
+  }
+
+  if (status === "error") {
+    return <ErrorAlert message={error.message} />;
+  }
+
+  const renderedCategories = data.map((category) => {
+    return <Item key={category.id} />;
+  });
+
   return (
     <>
       <h1>Список категорий</h1>
+      <div>{renderedCategories}</div>
     </>
   );
 }
