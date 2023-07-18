4d9bea630aa45fb039eb7dec237c7c3e0abd2145
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-18 22:29:15 +0500

    Добавит скрипт для создания бекапа

---
+++ make_backup.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+# docker exec -t fb_db pg_dump fatburger -U fatburger > backup.test
+#  && curl -X POST -H 'Content-Type: application/json' --data '{"chat_id": "'$CHAT_ID'", "text": "backup!"}' https://api.telegram.org/bot$TOKEN/sendMessage
+
+source .env
+
+fileName=/tmp/dump_`date +%Y-%m-%d"_"%H-%M-%S`.gz
+
+docker exec -t fb_db pg_dumpall -c -U $DB_NAME | gzip > $fileName && \
+curl -v -F "chat_id=$TELEGRAM_CHAT_ID" -F document=@"$fileName" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument && \
+rm $fileName
+
+# Restore
+# gunzip -c dump_2023-07-18_22-01-03.gz | docker exec -i fb_db psql -U fatburger
