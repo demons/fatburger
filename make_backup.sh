8d39f0ca14e9dd9ecbe026d5407462ba6f59dddc
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-18 23:20:01 +0500

    Изменит тип оболочки

--- make_backup.sh
+++ make_backup.sh
@@ -1,11 +1,11 @@
-#/bin/sh
+#/bin/bash
 
 # docker exec -t fb_db pg_dump fatburger -U fatburger > backup.test
 #  && curl -X POST -H 'Content-Type: application/json' --data '{"chat_id": "'$CHAT_ID'", "text": "backup!"}' https://api.telegram.org/bot$TOKEN/sendMessage
 
 source .env
 
-fileName=/tmp/dump_`date +%Y-%m-%d"_"%H-%M-%S`.gz
+fileName=/tmp/"$DB_NAME"_`date +%Y-%m-%d"_"%H-%M-%S`.gz
 
 docker exec -t fb_db pg_dumpall -c -U $DB_NAME | gzip > $fileName
 
