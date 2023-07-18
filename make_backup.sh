e9c47b06cdadaf6324c7cd2128672ebe5d2ad2ac
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2023-07-18 22:58:03 +0500

    Обновит make_backup.sh

--- make_backup.sh
+++ make_backup.sh
@@ -7,9 +7,12 @@ source .env
 
 fileName=/tmp/dump_`date +%Y-%m-%d"_"%H-%M-%S`.gz
 
-docker exec -t fb_db pg_dumpall -c -U $DB_NAME | gzip > $fileName && \
-curl -v -F "chat_id=$TELEGRAM_CHAT_ID" -F document=@"$fileName" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument && \
+docker exec -t fb_db pg_dumpall -c -U $DB_NAME | gzip > $fileName
+
+curl -v -F "chat_id=$TELEGRAM_CHAT_ID" -F document=@"$fileName" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument
+
 rm $fileName
 
 # Restore
-# gunzip -c dump_2023-07-18_22-01-03.gz | docker exec -i fb_db psql -U fatburger
+# file=dump_2023-07-18_20-51-29.gz
+# gunzip -c $file | docker exec -i fb_db psql -U fatburger
