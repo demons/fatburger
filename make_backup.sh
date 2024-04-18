b978c9ce6b12dfc3cac3fd866be5a7505670d89e
Aleksandr Shishkin <shishkin.aleksandr88@gmail.com>
2024-04-18 13:08:53 +0500

    Обновит скрипт создания бэкапа

--- make_backup.sh
+++ make_backup.sh
@@ -6,8 +6,16 @@
 source .env
 
 fileName=/tmp/"$DB_NAME"_`date +%Y-%m-%d"_"%H-%M-%S`.gz
+# docker exec -t fb_db pg_dumpall -c -U $DB_NAME | gzip > $fileName
 
-docker exec -t fb_db pg_dumpall -c -U $DB_NAME | gzip > $fileName
+# Создание бэкапа
+docker exec -t fb_db pg_dump --clean --create --if-exists -U fatburger fatburger | gzip > $fileName
+
+# Для восстановления сбрасываем все подключения к базе (чтобы не возникало ошибок)
+# SELECT pg_terminate_backend( pid ) FROM pg_stat_activity WHERE pid <>
+# pg_backend_pid( ) AND datname = 'fatburger';
+# Восстанавливаем базу данных из бэкапа
+# gunzip -c /tmp/fatburger_2024-04-18_11-11-27.gz | psql -U postgres -h localhost
 
 curl -v -F "chat_id=$TELEGRAM_CHAT_ID" -F document=@"$fileName" https://api.telegram.org/bot$TELEGRAM_TOKEN/sendDocument
 
